8366bbeb10860d315a7cbb8cd0b7ee7a1943258b
Mariusz Gwiazdecki <mariusz.gwiazdecki@outlook.com>
2021-08-03 21:41:56 +0200

    Add project files.

---
+++ AppointmentScheduling/DbInitializer/DbInitializer.cs
@@ -0,0 +1,57 @@
+﻿using AppointmentScheduling.Models;
+using AppointmentScheduling.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            if (_db.Roles.Any(x => x.Name == Helper.Admin)) return;
+
+            _roleManager.CreateAsync(new IdentityRole(Helper.Admin)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(Helper.Doctor)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(Helper.Patient)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new ApplicationUser { 
+                UserName = "email@address.com",
+                Email = "email@address.com",
+                EmailConfirmed = true,
+                Name = "Admin Admin"
+            }, "Admin123*").GetAwaiter().GetResult();
+
+            ApplicationUser user = _db.Users.FirstOrDefault(u => u.Email == "email@address.com");
+            _userManager.AddToRoleAsync(user, Helper.Admin).GetAwaiter().GetResult();
+
+        }
+    }
+}
---
+++ AppointmentScheduling/Migrations/20210722184252_AppointmentChange.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppointmentScheduling.Migrations
+{
+    public partial class AppointmentChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EndTime",
+                table: "Appointments",
+                newName: "EndDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EndDate",
+                table: "Appointments",
+                newName: "EndTime");
+        }
+    }
+}
